65b6260ae0a5d096419bd3113935c0f3d6d6b388
PranathiBadugu12 <pranathi12399@gmail.com>
2020-11-05 15:17:06 +0530

    "Added Employee,Main class"

---
+++ Core_Java_Application/src/com/app/org/MainClass.java
@@ -0,0 +1,9 @@
+package com.app.org;
+
+public class MainClass {
+	public static void main(String args[]) {
+		Employee e1 = new Employee(1,"pranathi",10000);
+		System.out.print(e1.toString());
+	}
+
+}
