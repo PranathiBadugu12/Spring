6580fc2e41c70454bd27e137b5a245cb3969ee55
PranathiBadugu12 <pranathi12399@gmail.com>
2020-11-11 16:01:30 +0530

    added

--- SpringExample/src/main/java/com/app/SpringExample/SpringExampleApplication.java
+++ SpringExample/src/main/java/com/app/SpringExample/SpringExampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
-        "com.app.SpringExample"})
+        "com.app.contoller"})
 public class SpringExampleApplication {
 
 	public static void main(String[] args) {
