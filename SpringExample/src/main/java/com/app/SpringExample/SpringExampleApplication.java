0fcf49bfe756b4733e9604d65c030bfdcd4c3ffc
PranathiBadugu12 <pranathi12399@gmail.com>
2020-11-11 15:51:03 +0530

    hhh

---
+++ SpringExample/src/main/java/com/app/SpringExample/SpringExampleApplication.java
@@ -0,0 +1,16 @@
+package com.app.SpringExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.app.SpringExample"})
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
